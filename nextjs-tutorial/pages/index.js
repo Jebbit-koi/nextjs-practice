b0d9dab6183ab4adbe956e2b7d5638c0b600e079
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2022-01-10 22:01:26 +0900

    add itemList page

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -1,14 +1,42 @@
 import Head from "next/head";
-import Image from "next/image";
+import Axios from "axios";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { Divider, Header, Loader } from "semantic-ui-react";
+import ItemList from "../src/component/ItemList";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+  function getData() {
+    Axios.get(API_URL).then((res) => {
+      console.log(res.data);
+      setList(res.data);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <Head>
         <title>HOME | 코이</title>
-        <meta name="description" content="코이 홈입니다."></meta>
       </Head>
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header as="h3" style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
     </div>
   );
 }
