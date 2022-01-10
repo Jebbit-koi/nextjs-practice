f94009ea4814d10e36c9f97ecf6ec55f27d72387
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2022-01-10 21:24:35 +0900

    first commit

---
+++ nextjs-tutorial/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>HOME | 코이</title>
+        <meta name="description" content="코이 홈입니다."></meta>
+      </Head>
+    </div>
+  );
+}
---
+++ nextjs-tutorial/src/component/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div>Copyright Jebbit-koi. All rights reserved.</div>;
+}
