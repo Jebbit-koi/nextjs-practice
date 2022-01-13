ed4e31dfc23c71409d9c83e36352103dc95969d8
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2022-01-13 11:46:40 +0900

    fetching data

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,11 +1,31 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "52a091be63099a3c18f3bac8adc8b270";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
