f63d511fcb632bc5c092955782e9ab1ad43a719d
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2022-01-27 11:16:51 +0900

    optimize images

--- nextjs-events-app/components/events/event-item.js
+++ nextjs-events-app/components/events/event-item.js
@@ -1,8 +1,9 @@
+import Image from "next/image";
+
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
 import DateIcon from "../icons/date-icon";
 import Button from "../ui/button";
-
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
@@ -18,7 +19,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
